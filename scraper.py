e0663b9c43cae59ef0df5d16b0d172d53893475b
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2022-09-16 14:16:49 +0300

    Stage 3

--- scraper.py
+++ scraper.py
@@ -1,19 +1,12 @@
 import requests
-from link_error import LinkError
-from bs4 import BeautifulSoup
 
 
-try:
+if __name__ == '__main__':
     url = input('Input the URL:\n')
-    if 'title' not in url or 'imdb' not in url:
-        raise LinkError
     r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    title = soup.find('h1').text
-    des = soup.find('span', {'data-testid': 'plot-l'}).text
-    if not des:
-        raise LinkError
-    movie = {'title': title, 'description': des}
-    print(movie)
-except LinkError as error:
-    print(error)
+    if not r:
+        print(f'The URL returned {r.status_code}!')
+        exit()
+    with open('source.html', 'wb') as f:
+        f.write(r.content)
+    print('\nContent saved.')
