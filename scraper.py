99f96f63b64b4d010130edf5bdc4bfa1544315c2
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2022-09-17 19:01:25 +0300

    Final

--- scraper.py
+++ scraper.py
@@ -1,27 +1,34 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 
 
-url = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3'
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
-all_articles = soup.find_all('article')
+page_number = int(input())
+art_type = input()
+url = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020'
 
-links = []
-for article in all_articles:
-    article_type = article.find('span', {'class': 'c-meta__type'}).text
-    if article_type == 'News':
-        links.append(article.find('a').get('href'))
-full_link = ['https://www.nature.com' + link for link in links]
-saved_articles = []
-for link in full_link:
-    r = requests.get(link)
-    soup_l = BeautifulSoup(r.content, 'html.parser')
-    l_content = soup_l.find('div', {'class': 'c-article-body'}).text.strip()
-    file_name = soup_l.find('title').text.replace(' ', '_') + '.txt'
-    saved_articles.append(file_name)
-    with open(file_name, 'wb') as f:
-        l_content_b = bytes(l_content, encoding='utf-8')
-        f.write(l_content_b)
+for i in range(1, page_number + 1):
+    os.mkdir(f'Page_{i}')
+    os.chdir(f'./Page_{i}')
+    r = requests.get(url, params={'page': f'{i}'})
 
-print(f'Saved articles: {saved_articles}')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    all_articles = soup.find_all('article')
+    links = []
+    for article in all_articles:
+        article_type = article.find('span', {'class': 'c-meta__type'}).text
+        if article_type == art_type:
+            links.append(article.find('a').get('href'))
+    full_link = ['https://www.nature.com' + link for link in links]
+
+    for link in full_link:
+        r = requests.get(link)
+        soup_l = BeautifulSoup(r.content, 'html.parser')
+        l_content = soup_l.find('div', {'class': 'c-article-body'}).text.strip()
+        file_name = soup_l.find('title').text.replace(' ', '_') + '.txt'
+        with open(file_name, 'wb') as f:
+            l_content_b = bytes(l_content, encoding='utf-8')
+            f.write(l_content_b)
+    os.chdir('..')
+
+print('Saved all articles.')
